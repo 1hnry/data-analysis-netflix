2bbba78c83722ef30763a0f36b9fa22824e8e627
Henry <bertagnahenry@gmail.com>
2025-08-02 12:33:36 -0300

    Update

--- Netflix Project/main_project.R
+++ Netflix Project/main_project.R
@@ -1,45 +1,50 @@
-netflix_csv <- read.csv("C:/Netflix Project/netflix_titles.csv")
-
-head(netflix_csv)
-
-glimpse(netflix_csv)
-
-names(netflix_csv)
+# DATA ANALYSIS PROJECT - NETFLIX CATALOG
+# by Henry Bertagna 
 
 
+
+# 1. INITIAL STEPS : Load the libraries:
+library(tidyverse)
+library(lubridate)
+library(showtext)
+library(ggthemes)
+library(patchwork)
+library(RColorBrewer)
+
+#2. DATA LOADING AND CLEANING
+#----------------------------------------------------------------------
+
+#Loading the dataset
+netflix_csv <- read.csv("data/netflix_titles.csv")
+
+
+#Load the font used in the project
 font_add_google("Rubik", "rubik")
 showtext_auto()
 
+#Returns the sum of all empty lines
 rowSums(is.na(netflix_csv))
 
-netflix_palette <- c(
-  "#FFFFFF",
-  "#8C8C8C",
-  "#580000",
-  "#B20710",
-  "#E50914"  
-)
-
-
+#Initial cleaning and creation of new columns
 netflix_cleaned <- netflix_csv %>% 
   filter(!is.na(release_year)) %>% 
   mutate(
     date_added = mdy(date_added),
     year_added = year(date_added),
-    duration_num = parse_number(duration),
-    duration_type = str_extract(duration, "[A-Za-z]+")
+    duration_num = parse_number(duration), #extracts only the number
+    duration_type = str_extract(duration, "[A-Za-z]+") #extracts characters
   ) 
   
-View(netflix_cleaned)
+#3. ANALYSIS AND VISUALIZATION
+#----------------------------------------------------------------------
 
+#Calculates the percentage of each type of content
 percent <- netflix_cleaned %>% 
   count(type) %>% 
   mutate(perc = n / sum(n),
          label = scales::percent(perc, accuracy = 0.1))
 
-font_add_google("Rubik", "rubik")
-showtext_auto()
-
+#First graph showing the percentage of each type of content
 ggplot(percent, aes(x = type, y = n, fill = type)) +
   geom_col(size = 0.6, show.legend = FALSE) + 
   geom_text(aes(label = label), vjust = 3.75, color="white", size = 7, family= "rubik", fontface="bold")+
@@ -61,6 +66,7 @@ ggplot(percent, aes(x = type, y = n, fill = type)) +
     panel.grid.minor = element_line(color = "gray20"),
     )
 
+#Second graph shows the number of contents added per year
 netflix_cleaned %>%
   filter(!is.na(year_added)) %>%         
   count(year_added) %>%
@@ -83,12 +89,14 @@ netflix_cleaned %>%
     axis.text.y = element_text(size = 12)
     )
 
+#Creates a table with the annual count of Movies and TV Shows added since 2011
 yearly_content_type <- netflix_cleaned %>% 
   filter(!is.na(year_added) & year_added >= 2011) %>% 
   group_by(year_added, type) %>% 
   summarise(qtd = n()) %>% 
   ungroup()
 
+#Third graph shows the number of movies and TV shows added per year
 ggplot(yearly_content_type, aes(x = year_added, y= qtd, color = type))+
   geom_line(size = 1.2) +
   geom_point(size = 2) +
@@ -116,7 +124,7 @@ ggplot(yearly_content_type, aes(x = year_added, y= qtd, color = type))+
         legend.title = element_text(color = "white", face = "bold", size = 11)
         )
 
-
+# Creates a count of titles by country, separating co-productions into individual lines
 country_count <- netflix_cleaned %>% 
   filter(!is.na(country)) %>% 
   separate_rows(country, sep = ",") %>% 
@@ -124,11 +132,9 @@ country_count <- netflix_cleaned %>%
   filter(country != "") %>% 
   count(country, sort = TRUE)
 
-head(country_count, 10)
 
-netflix_colors <- c("United States"="#E50914","India"="#B81D24","United Kingdom"="#A93226","Canada"="#922B21","France"="#831010", 
-                  "Japan"="#6A1010","Spain"="#5A1515","South Korea"="#4A1A1A","Germany"="#3A3A3A","Mexico"="#2A2A2A")
 
+#Fourth graph shows the top 10 countries with the most content
 country_count %>%  
   slice_max(n, n = 10) %>% 
   ggplot(aes(x = reorder(country, n), y = n, fill = country))+
@@ -149,14 +155,14 @@ country_count %>%
         axis.text.y = element_text(size = 12)
         )
 
+#Creates a filtered and organized table that returns the count of each genre
 listed <- netflix_cleaned %>% 
   filter(!is.na(listed_in)) %>% 
   separate_rows(listed_in, sep = ",") %>%
   mutate(listed_in = str_trim(listed_in)) %>% 
   count(listed_in, sort = TRUE)
 
-head(listed, 10)
-  
+#Fifth graph shows the top 10 most common genres
 listed %>% 
   slice_max(n , n =10) %>% 
   ggplot(aes(x = reorder(listed_in, n), y = n)) +
@@ -177,7 +183,7 @@ listed %>%
         axis.text.y = element_text(size = 12),
         )
 
-
+#Create a table that counts the amount of content added per year by each country
 country_year <- netflix_cleaned %>% 
   filter(!is.na(country), !is.na(year_added)) %>% 
   separate_rows(country, sep = ",") %>% 
@@ -185,15 +191,17 @@ country_year <- netflix_cleaned %>%
   filter(country != "") %>% 
   count(year_added, country, sort = TRUE)
 
+
+#In this section I chose 5 countries to analyze, 2 Western and 3 Eastern
 top_countries <- c("United States", "India", "United Kingdom", "South Korea", "Japan")
 
 country_year_top <- country_year %>%
   filter(country %in% top_countries)
 
+#Palette used for this graphic
 myColors = c("#5e0308","#914747", "gray88", "#E50914", "#2b0000" )
 
-
-
+#The sixth graph shows the evolution of bonds added per year by each country.
 country_year_top %>% 
   ggplot(aes(x =  year_added, y = n, color = country)) +
   geom_line(size = 1.5, alpha = 0.8) +
@@ -217,31 +225,30 @@ country_year_top %>%
         legend.title = element_text(color = "white", face = "bold", size = 11)
         )
 
-
-
+#Separates only valid ratings for analysis
 valid_ratings <- c("TV-MA", "R", "PG-13", "TV-14", "PG", "TV-PG", "TV-Y", "TV-Y7", "TV-G", "G", "NC-17", "NR", "UR")
 
 netflix_ratings_cleaned <- netflix_cleaned %>%
   filter(rating %in% valid_ratings)
 
+#Take only the top 7 ratings
 top_ratings_cleaned <- netflix_ratings_cleaned %>%
   count(rating, sort = TRUE) %>%
   slice_max(n, n = 7) %>%
   pull(rating)
 
+#Creates a table that counts each rating
 rating_by_type_cleaned <- netflix_ratings_cleaned %>%
-  
   filter(rating %in% top_ratings_cleaned) %>%
   count(type, rating) %>%
   group_by(type) %>%
   mutate(percent = n / sum(n)) %>%
-  
   mutate(
     rating = factor(rating, levels = c("TV-MA", "R", "PG-13", "TV-14", "PG", "TV-PG", "TV-Y", "TV-G", "TV-Y7"))
   )
 
 
-
+#Palette used for this graphic
 test_palette <- c(
   "TV-MA" = "#831010",   
   "R" = "#B20710",      
@@ -253,6 +260,7 @@ test_palette <- c(
   "TV-G" = "#F5F5F1"    
 )
 
+#The seventh graph shows the distribution of each rating by type (Movies and TV shows).
 ggplot(rating_by_type_cleaned, aes(x = type, y=percent , fill = rating)) +
   geom_col() +
   scale_fill_manual(values = test_palette) +
@@ -274,15 +282,18 @@ ggplot(rating_by_type_cleaned, aes(x = type, y=percent , fill = rating)) +
         legend.text = element_text(color = "white", size = 10), 
         legend.title = element_text(color = "white", face = "bold", size = 11)
         )
-      
+
+#Take the top 8 countries with the most content
 top_8_countries <- country_count %>% 
   slice_max(n, n=8) %>% 
   pull(country)
 
+#Take the top 10 most common genres
 top_10_genres <- listed %>% 
   slice_max(n, n =10) %>% 
   pull(listed_in)
 
+#Create a table that prepares the heatmap for Countries vs. Genres
 country_genre_data <- netflix_cleaned %>% 
   filter(!is.na(country) & !is.na(listed_in)) %>%
   separate_rows(country, sep = ", ") %>%
@@ -296,7 +307,7 @@ country_genre_data <- netflix_cleaned %>%
     listed_in = fct_reorder(listed_in, n, .fun = 'sum', .desc = TRUE)
 )
 
-
+#Eighth graph shows the heatmap
 ggplot(country_genre_data, aes(x=listed_in, y=country, fill = n)) +
   geom_tile(color = "white", lwd = 0.5) +
   geom_text(aes(label = n), color = "black", size = 3) +
@@ -323,6 +334,7 @@ ggplot(country_genre_data, aes(x=listed_in, y=country, fill = n)) +
     panel.grid.minor = element_line(color = "gray20")
   )
 
+#Prepares the data for the monthly timeline graph
 monthly_additions <- netflix_cleaned %>% 
   filter(!is.na(date_added)) %>% 
   mutate(
@@ -333,6 +345,7 @@ monthly_additions <- netflix_cleaned %>%
   mutate(date = make_date(year = year_added, month = month_added, day = 1)) %>% 
   filter(date >= "2015-01-01")
 
+#Ninth graph shows content added per month
 ggplot(monthly_additions, aes(x = date, y = count)) +
   geom_line(color = "#B20710", size = 1) +
   geom_point(color = "#B20710", alpha = 0.5)+
@@ -363,16 +376,18 @@ ggplot(monthly_additions, aes(x = date, y = count)) +
     axis.text.y = element_text(size = 12)
   )
 
-
+#Calculates the 10 most common series lengths (in number of seasons)
 seasons_count <- netflix_cleaned %>% 
   filter(type == "TV Show") %>% 
   count(duration, sort = TRUE) %>% 
   mutate(num_seasons = parse_number(duration)) %>% 
   slice_max(order_by = n, n =10)
 
+#Separate only the top 10 amounts of seasons
 top_10_seasons <- seasons_count %>% 
   slice_max(n, n =10)
 
+#Tenth graph shows the number of the most common seaons in series
 tvshows_top10 <- ggplot(top_10_seasons, aes(x = reorder(duration, num_seasons), y = n))+
   geom_col(fill = "#B20710", show.legend = FALSE) +
   geom_text(aes(label = n), hjust = -0.2, color = "#5e0308", size = 4, fontface = "bold") +
@@ -396,15 +411,9 @@ tvshows_top10 <- ggplot(top_10_seasons, aes(x = reorder(duration, num_seasons), 
     axis.text.y = element_text(size = 12)
   )
 
-avg_movie_duration <- netflix_cleaned %>% 
-  filter(type == "Movie") %>% 
-  summarise(mean_duration = mean(duration_num, na.rm = TRUE)) %>% 
-  pull(mean_duration)
 
-rounded_avg <- round(avg_movie_duration)
-  
-tvshows_top10
 
+#Eleventh graph shows the minute count of the movies
 plot_movies <- netflix_cleaned %>% 
   filter(type == "Movie", !is.na(duration_num)) %>% 
   ggplot(aes(x = duration_num)) +
@@ -424,13 +433,20 @@ plot_movies <- netflix_cleaned %>%
   scale_x_continuous(breaks = seq(0 , 250, by = 30))+
   coord_cartesian(xlim = c(0,250))
 
-plot_movies
-           
+#Calculates the average minutes of movies
+avg_movie_duration <- netflix_cleaned %>% 
+  filter(type == "Movie") %>% 
+  summarise(mean_duration = mean(duration_num, na.rm = TRUE)) %>% 
+  pull(mean_duration)
+
+rounded_avg <- round(avg_movie_duration)
+
+
+#Combine the two graphs in a single dashboard           
 duration_dashboard <- tvshows_top10 + plot_movies
 
 
-duration_dashboard
-
+#Adjust the dashboard for visualization
 final_dashboard <- duration_dashboard + plot_annotation(
   title = "Predictable Movies, Disposable Series?", 
   subtitle = "The format of the series favors short productions, while the films follow a cinematic pattern.",
@@ -442,4 +458,5 @@ final_dashboard <- duration_dashboard + plot_annotation(
     plot.caption = element_text(size = 10, family = "rubik", hjust = 0.9)
   ))
   
+
 final_dashboard
